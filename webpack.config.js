59d1b3638776ed9ef22636650e793ac4cb5849a2
Timur_etc <166824017+TimurETC@users.noreply.github.com>
2024-04-12 18:21:57 +0500

    Add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'compressed-images.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin()
+  ]
+};
